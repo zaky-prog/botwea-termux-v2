2f0de1619673cf28e014f6be55b39626d9f483bc
Anker production <67215583+4NK3R-PRODUCT1ON@users.noreply.github.com>
2021-01-26 13:55:06 +0700

    Ubah ae serah lu #:v

---
+++ database/menu/group.js
@@ -0,0 +1,79 @@
+const group = (pushname, prefix, botName, ownerName, reqXp, uangku) => {
+        return `
+╔══✪〘 INFORMATION 〙✪══
+║
+║╔═══════════════════╗
+║║╠➥ Name : ${botName}
+║║╠➥3.0
+║║╠➥ wa.me/6281368646011
+║║╠➥ Prefix: 「  ${prefix}  」
+║║╠➥ Creator: ${ownerName}
+║║╠➥ Name: ${pushname}
+║║╠➥ XP: ${reqXp}
+║║╠➥ Money: ${uangku}
+║║╠➥ Registered: ✔️
+║╚═══════════════════╝
+║
+║═══°❀ ❬ *DARK BOT* ❭ ❀°═══⊱
+║
+║╔═══════════════════╗
+║║╠➥ *${prefix}info*
+║║╠➥ *${prefix}blocklist*
+║║╠➥ *${prefix}chatlist*
+║║╠➥ *${prefix}ping*
+║║╠➥ *${prefix}bugreport*
+║║╠➥ *${prefix}opengc*
+║║╠➥ *${prefix}closegc*
+║║╠➥ *${prefix}promote*
+║║╠➥ *${prefix}demote*
+║║╠➥ *${prefix}tagall*
+║║╠➥ *${prefix}tagall2*
+║║╠➥ *${prefix}tagall3*
+║║╠➥ *${prefix}tagall4*
+║║╠➥ *${prefix}tagall5*
+║║╠➥ *${prefix}add*
+║║╠➥ *${prefix}kick*
+║║╠➥ *${prefix}listadmins*
+║║╠➥ *${prefix}linkgroup*
+║║╠➥ *${prefix}leave*
+║║╠➥ *${prefix}welcome*
+║║╠➥ *${prefix}nsfw*
+║║╠➥ *${prefix}delete*
+║║╠➥ *${prefix}simih*
+║║╠➥ *${prefix}ownergroup*
+║║╠➥ *Ada Fitur Maker Eror Di Sebabkan oleh web down/eror*
+║╚═══════════════════╝
+║
+║══✪〘 IKLAN 〙✪══
+║
+║╔═════════════════════════════╗
+║║╠➥ *DAFTAR SEWA & BUAT BOT :*
+║║╠➥ *SEWA : 20K/GRUP (BULAN)*
+║║╠➥ *BUAT : 30K (BISA JADI OWNER)*
+║║╠➥ *PEMBAYARAN BISA MELALUI :*
+║║╠➥ *GOPAY, PULSA +5K*
+╠══════════════✪✪════════════
+║║╠➥ *KEUNTUNGAN SEWA BOT :*
+║║╠➥ *1. BISA MENJADI VIP DARK*
+║║╠➥ *2. BISA MENDAPATKAN COMMAND VIP*
+║║╠➥ *KEUNTUNGAN BUAT BOT :*
+║║╠➥ *1. BISA MENJADI OWNER BOT SENDIRI*
+║║╠➥ *2. BISA MENGGANTI NAMA BOT SENDIRI*
+║║╠➥ *3. BISA MEMBAWA BOT KE GROUP*
+║║╠➥ *4. BISA MENGGUNAKAN COMMAND OWNER*
+║║╠➥ *5. BISA MENYEWAKAN BOT KEMBALI*
+║╚═════════════════════════════╝
+║➥ *JIKA MINAT IKLAN DIATAS*
+║➥ *HARAP HUBUNGI NOMOR DIBAWAH :*
+║➥ *wa.me/6281368646011*
+║
+║╔═════════════════════════════╗
+║║╠➥ *DONASI BISA MELALUI :*
+║║╠➥ *PULSA/GOPAY : 081368646011*
+║║╠➥ *SAWERIA : https://saweria.co/darkbot2412*
+║║╠➥ *TERIMA KASIH BANYAK YANG SUDAH MAU DONASI*
+║╚═════════════════════════════╝
+║
+╚═〘 DARK BOT 〙`
+}
+exports.group = group
---
+++ database/menu/xp.js
@@ -0,0 +1,63 @@
+const xp = (prefix, botName, ownerName) => {
+        return `
+╔══✪〘 INFORMATION 〙✪══
+║
+║╔═══════════════════╗
+║║╠➥ Name : ${botName}
+║║╠➥3.0
+║║╠➥ wa.me/6281368646011
+║║╠➥ Prefix: 「  ${prefix}  」
+║║╠➥ Creator: ${ownerName}
+║║╠➥ Name: ${pushname}
+║║╠➥ XP: ${reqXp}
+║║╠➥ Money: ${uangku}
+║║╠➥ Registered: ✔️
+║╚═══════════════════╝
+║
+║═══°❀ ❬ *DARK BOT* ❭ ❀°═══⊱
+║
+║╔═══════════════════╗
+║║╠➥ *${prefix}info*
+║║╠➥ *${prefix}blocklist*
+║║╠➥ *${prefix}chatlist*
+║║╠➥ *${prefix}ping*
+║║╠➥ *${prefix}bugreport*
+║║╠➥ ${prefix}leveling
+║║╠➥ ${prefix}level
+║║╠➥ ${prefix}mining
+║║╠➥ *Note: Kamu bisa mengumpulkan xp dan menaikan levelmu dengan cara chat dengan siapapun di dalam grup yang telah di aktifkan fitur leveling nya.*
+║╚═══════════════════╝
+║
+║══✪〘 IKLAN 〙✪══
+║
+║╔═════════════════════════════╗
+║║╠➥ *DAFTAR SEWA & BUAT BOT :*
+║║╠➥ *SEWA : 20K/GRUP (BULAN)*
+║║╠➥ *BUAT : 30K (BISA JADI OWNER)*
+║║╠➥ *PEMBAYARAN BISA MELALUI :*
+║║╠➥ *GOPAY, PULSA +5K*
+╠══════════════✪✪════════════
+║║╠➥ *KEUNTUNGAN SEWA BOT :*
+║║╠➥ *1. BISA MENJADI VIP DARK*
+║║╠➥ *2. BISA MENDAPATKAN COMMAND VIP*
+║║╠➥ *KEUNTUNGAN BUAT BOT :*
+║║╠➥ *1. BISA MENJADI OWNER BOT SENDIRI*
+║║╠➥ *2. BISA MENGGANTI NAMA BOT SENDIRI*
+║║╠➥ *3. BISA MEMBAWA BOT KE GROUP*
+║║╠➥ *4. BISA MENGGUNAKAN COMMAND OWNER*
+║║╠➥ *5. BISA MENYEWAKAN BOT KEMBALI*
+║╚═════════════════════════════╝
+║➥ *JIKA MINAT IKLAN DIATAS*
+║➥ *HARAP HUBUNGI NOMOR DIBAWAH :*
+║➥ *wa.me/6281368646011*
+║
+║╔═════════════════════════════╗
+║║╠➥ *DONASI BISA MELALUI :*
+║║╠➥ *PULSA/GOPAY : 081368646011*
+║║╠➥ *SAWERIA : https://saweria.co/darkbot2412*
+║║╠➥ *TERIMA KASIH BANYAK YANG SUDAH MAU DONASI*
+║╚═════════════════════════════╝
+║
+╚═〘 DARK BOT 〙`
+}
+exports.xp = xp
