e5cc3e24a1437ba4e692e1fdf6328e478eebad25
Anker production <67215583+4NK3R-PRODUCT1ON@users.noreply.github.com>
2021-01-27 14:36:35 +0700

    p

--- database/menu/stalk.js
+++ database/menu/stalk.js
@@ -1,4 +1,4 @@
-const stalk = (pushname, prefix, botName, ownerName, reqXp, uangku) => {
+const stalk = (pushname, prefix, botName, ownerName) => {
         return `
 ╔══✪〘 INFORMATION 〙✪══
 ║
@@ -9,8 +9,6 @@ const stalk = (pushname, prefix, botName, ownerName, reqXp, uangku) => {
 ║║╠➥ Prefix: 「  ${prefix}  」
 ║║╠➥ Creator: ${ownerName}
 ║║╠➥ Name: ${pushname}
-║║╠➥ XP: ${reqXp}
-║║╠➥ Money: ${uangku}
 ║║╠➥ Registered: ✔️
 ║╚═══════════════════╝
 ║
