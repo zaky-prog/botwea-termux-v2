db4bb00bc2135245e0ee4c2b287ade9a33a5cda6
Anker production <67215583+4NK3R-PRODUCT1ON@users.noreply.github.com>
2021-01-27 14:34:28 +0700

    p

--- database/menu/imagemaker.js
+++ database/menu/imagemaker.js
@@ -1,4 +1,4 @@
-const imagemaker = (pushname, prefix, botName, ownerName, reqXp, uangku) => {
+const imagemaker = (pushname, prefix, botName, ownerName) => {
         return `
 ╔══✪〘 INFORMATION 〙✪══
 ║
@@ -9,8 +9,6 @@ const imagemaker = (pushname, prefix, botName, ownerName, reqXp, uangku) => {
 ║║╠➥ Prefix: 「  ${prefix}  」
 ║║╠➥ Creator: ${ownerName}
 ║║╠➥ Name: ${pushname}
-║║╠➥ XP: ${reqXp}
-║║╠➥ Money: ${uangku}
 ║║╠➥ Registered: ✔️
 ║╚═══════════════════╝
 ║
