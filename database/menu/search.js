dfbb2d4e93c2e41995ec6deff1ed7bcc0ddf4413
Anker production <67215583+4NK3R-PRODUCT1ON@users.noreply.github.com>
2021-01-27 14:36:16 +0700

    p

--- database/menu/search.js
+++ database/menu/search.js
@@ -1,4 +1,4 @@
-const search = (pushname, prefix, botName, ownerName, reqXp, uangku) => {
+const search = (pushname, prefix, botName, ownerName) => {
 	return `
 ╔══✪〘 INFORMATION 〙✪══
 ║
@@ -9,8 +9,6 @@ const search = (pushname, prefix, botName, ownerName, reqXp, uangku) => {
 ║║╠➥ Prefix: 「  ${prefix}  」
 ║║╠➥ Creator: ${ownerName}
 ║║╠➥ Name: ${pushname}
-║║╠➥ XP: ${reqXp}
-║║╠➥ Money: ${uangku}
 ║║╠➥ Registered: ✔️
 ║╚═══════════════════╝
 ║
